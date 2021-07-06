73cf2fe67a80aeeb6668ff8e269fd250bd010d0f
Pepe <saraiptr@gmail.com>
2021-07-06 13:44:09 +0200

    Added client language Added php-fpm default options

--- SPeter/ISPConfig/API/REST/Entity/WebDomain.php
+++ SPeter/ISPConfig/API/REST/Entity/WebDomain.php
@@ -47,15 +47,22 @@ class WebDomain extends Base
         'allow_override' => 'All',
         'apache_directives' => '',
         'php_open_basedir' => '/',
-        'pm_max_requests' => 0,
+        'pm' => 'dynamic',
+        'php_fpm_use_socket' => 'n',
+        'pm_max_children' => 10,
+        'pm_start_servers' => 2,
+        'pm_min_spare_servers' => 1,
+        'pm_max_spare_servers' => 5,
+        'pm_max_requests' => 1000,
         'pm_process_idle_timeout' => 10,
         'custom_php_ini' => '',
-        'backup_interval' => '',
+        'backup_interval' => 'monthly',
         'backup_copies' => 1,
         'active' => 'n',
         'traffic_quota_lock' => 'n',
         'http_port' => '80',
-        'https_port' => '443'
+        'https_port' => '443',
+        'fastcgi_php_version' => 'PHP7.2:/opt/php-7.2/bin/php-cgi:/opt/php-7.2/lib'
     ];
 
     public function setPHP($handler)
